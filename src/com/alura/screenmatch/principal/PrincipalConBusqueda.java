adb55342ad2ca20db7a5f060e69209f3e3acd1d4
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-11-11 11:35:52 -0300

    Video 1.5

--- src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula: ");
+        var busqueda = lectura.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=d4d0bf92";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=d4d0bf92"))
+                .uri(URI.create(direccion))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
